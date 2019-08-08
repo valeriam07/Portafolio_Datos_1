b6bf6da0ad4c13f84b79ea0d3ed5464902f7b004
valeriam07 <valeriamorales40@gmail.com>
2019-08-08 16:23:09 -0600

    Ejemplo usando Throw, Try y Finally

--- src/Tarea2/Ejemplos.java
+++ src/Tarea2/Ejemplos.java
@@ -2,10 +2,26 @@ package Tarea2;
 
 import java.util.Scanner;
 
-//La función pide un numerador y un denominador para hacer la division respectiva, siempre y cuando el resultado sea
-// menor a 10, pide un numero para realizar una operacion
+//La función pide un numerador y un denominador para hacer la division respectiva.
+// Posteriormente, siempre y cuando el resultado sea menor a 10, pide un numero para realizar una suma
 
 public class Ejemplos {
+    private static int sum;
+
+    public static void sumar(int res) {
+
+        try {
+            System.out.println("Ingrese la cantidad que desea sumar");
+            Scanner entrada = new Scanner(System.in);
+            sum = entrada.nextInt() + res;
+            System.out.println("El resultado de la suma es: " + sum);
+
+
+        } catch (ArithmeticException e) {
+            System.out.println("Ocurrió un error: " + e);
+        }
+
+    }
 
     public static void main(String[] args) {
 
@@ -16,17 +32,21 @@ public class Ejemplos {
             int denominador = entrada.nextInt();
 
             try {
-                int res = 0;
-                res = numerador / denominador;
+                int res = numerador / denominador;
                 System.out.println("El resultado de la división es: " + res);
-                throw new ArithmeticException("res>10");
+                sumar(res);
+
+                if (sum > 10) {
+                    throw new ArithmeticException("resultado > 10");
+                }
+
 
 
             } catch (ArithmeticException e) {
                 System.out.println("Ocurrió un error: "+ e);
 
             }finally {
-                System.out.println("Terminó la ejecucion del código");
+                System.out.println("Terminó la ejecución del código");
             }
         }
 
