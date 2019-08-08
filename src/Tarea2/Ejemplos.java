e1284e44a76d0594e761dfd60d72d2aaecbeae86
valeriam07 <valeriamorales40@gmail.com>
2019-08-08 16:25:15 -0600

    Ejemplo usando Throw, Try y Finally

--- src/Tarea2/Ejemplos.java
+++ src/Tarea2/Ejemplos.java
@@ -16,38 +16,34 @@ public class Ejemplos {
             sum = entrada.nextInt() + res;
             System.out.println("El resultado de la suma es: " + sum);
 
-
         } catch (ArithmeticException e) {
             System.out.println("Ocurrió un error: " + e);
         }
-
     }
 
     public static void main(String[] args) {
 
-            Scanner entrada = new Scanner(System.in);
-            System.out.println("Ingrese un numerador");
-            int numerador = entrada.nextInt();
-            System.out.println("Ingrese un denominador");
-            int denominador = entrada.nextInt();
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese un numerador");
+        int numerador = entrada.nextInt();
+        System.out.println("Ingrese un denominador");
+        int denominador = entrada.nextInt();
 
-            try {
-                int res = numerador / denominador;
-                System.out.println("El resultado de la división es: " + res);
-                sumar(res);
+        try {
+            int res = numerador / denominador;
+            System.out.println("El resultado de la división es: " + res);
+            sumar(res);
 
-                if (sum > 10) {
-                    throw new ArithmeticException("resultado > 10");
-                }
-
-
-
-            } catch (ArithmeticException e) {
-                System.out.println("Ocurrió un error: "+ e);
-
-            }finally {
-                System.out.println("Terminó la ejecución del código");
+            if (sum > 10) {
+                throw new ArithmeticException("resultado > 10");
             }
-        }
 
+        } catch (ArithmeticException e) {
+            System.out.println("Ocurrió un error: " + e);
+
+        } finally {
+            System.out.println("Terminó la ejecución del código");
+        }
     }
+
+}
